b021d122aaa1338b2ddc47869d24119a6ea8c5fe
DARSHANA GOHIL <99174264+darshu1206@users.noreply.github.com>
2025-01-15 12:39:45 +0000

    stack and heap added

--- 01_basics/dataTypes-Summary.js
+++ 01_basics/dataTypes-Summary.js
@@ -35,3 +35,33 @@ const myFunction = function(){
 console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ====================================================
+
+// Stack (Primitive Data Types are stored in Stack Memory) 
+// Heap (Non-Primitive Data Types are stored in Heap Memory)
+
+let myYoutubeName = "darshanagohildotcom"
+
+let anotherName = myYoutubeName
+
+anotherName = "darshudotcom"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+//In Stack, it will make the copy of it and if we change then it will be done in the copied variable insted of the original variable
+
+let userOne = {
+    email: "darshu@gmail.com",
+    age: 22
+}
+
+let userTwo = userOne
+
+userTwo.email = "darshana@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// In Heap, Variable Name is stored in to the Stack Memory and it's data is stored in Heap. If we make the reference of it and try to change the data then it will modify it 
